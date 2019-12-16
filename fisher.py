40f7c4b3bd8ade36fda2bfca1efc35d5c84c32ad
ttkltll <ttkltll@163.com>
2019-12-16 11:52:49 +0800

    from app.web import book

--- fisher.py
+++ fisher.py
@@ -6,6 +6,7 @@ app = Flask(__name__)
 app.config.from_object('config')
 print(app.config['DEBUG'])
 
+from app.web import book
 
 if __name__ == '__main__':
     app.run(debug=app.config['DEBUG'], host='0.0.0.0', port=81)
