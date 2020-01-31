4cdf5f8b180914df86d19e15c8838853fdbe90f6
ttkltll <ttkltll@163.com>
2020-01-31 16:00:08 +0800

    写好了save_to_wish和save_to_gift

--- app/models/base.py
+++ app/models/base.py
@@ -1,9 +1,29 @@
-import datetime
+from flask_sqlalchemy import SQLAlchemy as _SQLAlchemy, BaseQuery
+from sqlalchemy import Column, Integer, SmallInteger
+from contextlib import contextmanager
 
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import Column, SmallInteger, Integer
 
-db = SQLAlchemy()
+class SQLAlchemy(_SQLAlchemy):
+    @contextmanager
+    def auto_commit(self):
+        try:
+            yield
+            self.session.commit()
+        except Exception as e:
+            db.session.rollback()
+            raise e
+
+
+class Query(BaseQuery):
+    def filter_by(self, **kwargs):
+        if 'status' not in kwargs.keys():
+            kwargs['status'] = 1
+        return super(Query, self).filter_by(**kwargs)
+
+
+db = SQLAlchemy(query_class=Query)
+
+
 
 class Base(db.Model):
     __abstract__ = True
--- app/models/user.py
+++ app/models/user.py
@@ -11,6 +11,7 @@ from flask_login import UserMixin
 from app import login_manager
 from app.libs.helper import is_isbn_or_key
 from app.models.gift import Gift
+from app.models.wish import Wish
 from app.spider.yushu_book import YuShuBook
 
 class User(UserMixin, Base):
@@ -37,6 +38,28 @@ class User(UserMixin, Base):
 
     def check_password(self, raw):
         return check_password_hash(self._password, raw)
+
+    def can_save_to_list(self, isbn):
+        if is_isbn_or_key(isbn) != 'isbn':
+            return False
+        yushu_book = YuShuBook()
+        yushu_book.search_by_isbn(isbn)
+        if not yushu_book.first:
+            return False
+        # 不允许一个用户同时赠送多本相同的图书
+        # 一个用户不可能同时成为赠送者和索要者
+
+        # 既不在赠送清单，也不在心愿清单才能添加
+        gifting = Gift.query.filter_by(uid=self.id, isbn=isbn,
+                                       launched=False).first()
+        wishing = Wish.query.filter_by(uid=self.id, isbn=isbn,
+                                       launched=False).first()
+
+        if not gifting and not wishing:
+            return True
+        else:
+            return False
+
 
 @login_manager.user_loader
 def get_user(uid):
